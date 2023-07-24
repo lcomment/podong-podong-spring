f926086f549521a7b1bd55780b0aa27133c40984
lcomment <kohyunsuk98@gmail.com>
2023-07-24 14:37:34 +0900

    feature: init project

---
+++ src/main/kotlin/com/podongpodong/Application.kt
@@ -0,0 +1,11 @@
+package com.podongpodong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PodongPodongApplication
+
+fun main(args: Array<String>) {
+	runApplication<PodongPodongApplication>(*args)
+}
