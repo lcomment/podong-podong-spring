47cf5d687da101f2faf23f4afdc0006338b66e4f
lcomment <kohyunsuk98@gmail.com>
2023-07-26 13:57:27 +0900

    feature: OAuth2에 필요한 Converter 클래스 및 Dto 구현

---
+++ src/main/kotlin/com/podongpodong/global/oauth2/converter/AttributeConverterFactory.kt
@@ -0,0 +1,28 @@
+package com.podongpodong.global.oauth2.converter
+
+import com.podongpodong.domain.user.entity.enums.Provider
+import com.podongpodong.global.oauth2.converter.attribute.AttributeConverter
+import com.podongpodong.global.oauth2.converter.attribute.KakaoAttributeConverter
+
+import org.springframework.stereotype.Component
+
+import java.util.EnumMap
+
+@Component
+class AttributeConverterFactory(
+    private val kakaoAttributeConverter: KakaoAttributeConverter
+) {
+    private val converterMap: MutableMap<Provider, AttributeConverter> = EnumMap(Provider::class.java)
+
+    init {
+        initialize()
+    }
+
+    private fun initialize() {
+        converterMap[Provider.KAKAO] = kakaoAttributeConverter;
+    }
+
+    fun getConverterByProvider(provider: Provider): AttributeConverter? {
+        return converterMap[provider]
+    }
+}
