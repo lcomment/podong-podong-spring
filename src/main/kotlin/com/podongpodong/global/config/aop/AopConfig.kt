5aac8fd6d63672d7a24ff64ab30bea786afcbb3f
lcomment <kohyunsuk98@gmail.com>
2023-07-26 17:25:25 +0900

    chore: AOP 및 로그 설정

---
+++ src/main/kotlin/com/podongpodong/global/config/aop/AopConfig.kt
@@ -0,0 +1,9 @@
+package com.podongpodong.global.config.aop
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@Configuration
+@EnableAspectJAutoProxy
+class AopConfig {
+}
