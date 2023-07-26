89fa42a589d5170dd0a07051e44f727bddada6d9
lcomment <kohyunsuk98@gmail.com>
2023-07-26 16:15:42 +0900

    refactor: 공통 응답 적용

--- src/main/kotlin/com/podongpodong/global/oauth2/handler/CustomAuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/podongpodong/global/oauth2/handler/CustomAuthenticationSuccessHandler.kt
@@ -1,9 +1,12 @@
 package com.podongpodong.global.oauth2.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.podongpodong.global.common.ApiResponse
+import com.podongpodong.global.common.createSuccessWithData
 import com.podongpodong.global.jwt.dto.JwtToken
 import com.podongpodong.global.jwt.util.JwtProvider
 import com.podongpodong.global.oauth2.dto.PrincipalUser
+import com.podongpodong.global.oauth2.dto.enum.AuthResponse
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -24,8 +27,12 @@ class CustomAuthenticationSuccessHandler(
     ) {
         val principalUser: PrincipalUser = authentication.principal as PrincipalUser
         val jwtToken: JwtToken = jwtProvider.createJwtToken(principalUser)
+        val apiResponse: ApiResponse<JwtToken> = createSuccessWithData(
+            AuthResponse.SUCCESS_GENERATE_TOKEN.msg,
+            jwtToken
+        )
 
         response.contentType = "application/json";
-        response.writer.write(objectMapper.writeValueAsString(jwtToken))
+        response.writer.write(objectMapper.writeValueAsString(apiResponse))
     }
 }
