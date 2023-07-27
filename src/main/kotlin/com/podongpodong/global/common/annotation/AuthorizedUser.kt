dd84ad1fafb4cb342b5405c13bcf8eb5085eda94
lcomment <kohyunsuk98@gmail.com>
2023-07-27 14:52:24 +0900

    feature: AuthorizedUser 어노테이션 및 resolver 구현 및 적용

---
+++ src/main/kotlin/com/podongpodong/global/common/annotation/AuthorizedUser.kt
@@ -0,0 +1,5 @@
+package com.podongpodong.global.common.annotation
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AuthorizedUser()
---
+++ src/main/kotlin/com/podongpodong/global/config/mvc/WebMvcConfig.kt
@@ -0,0 +1,16 @@
+package com.podongpodong.global.config.mvc
+
+import com.podongpodong.domain.user.service.UserService
+import com.podongpodong.global.common.resovler.AuthorizedUserResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig(
+    private val userService: UserService
+) : WebMvcConfigurer {
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(AuthorizedUserResolver(userService))
+    }
+}
